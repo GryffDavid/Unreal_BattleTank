1077384c331470d4ec3d2a4a2eeb13f878e3c7c2
Gryff David <gryffdavid@gmail.com>
2020-11-16 21:39:34 +0200

    Forces are applied to the Tank Tracks. Still have some friction stuff to work out, but a force is being applied.

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -3,3 +3,14 @@
 
 #include "TankTrack.h"
 
+void UTankTrack::SetThrottle(float throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%f throttle"), throttle);
+
+	FVector ForceApplied = GetForwardVector() * throttle * MaxTrackDrivingForce;
+	FVector TrackLocation = GetComponentLocation();
+
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, TrackLocation);
+
+}
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -7,11 +7,21 @@
 #include "TankTrack.generated.h"
 
 /**
- * 
+ * Tank Track is used to set driving forces on the tank
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float throttle);
+
+	//Force on this track in newtons
+	UPROPERTY(EditDefaultsOnly, Category = "Track")
+	float MaxTrackDrivingForce = 400000;
+
+//private:
 	
 };
